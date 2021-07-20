1073ac1a8df122606c2cfd77e143573c74f9968c
Julien Guégan <julienguegan56520@gmail.com>
2021-07-20 20:20:40 +0200

    update to starter pack

---
+++ vendor/bundle/ruby/2.5.0/gems/jekyll-spaceship-0.9.9/lib/jekyll-spaceship/processors/emoji-processor.rb
@@ -0,0 +1,87 @@
+# frozen_string_literal: true
+
+require 'net/http'
+require 'json'
+require 'gemoji'
+
+module Jekyll::Spaceship
+  class EmojiProcessor < Processor
+    def self.config
+      {
+        'css' => {
+          'class' => 'emoji'
+        },
+        'src' => 'https://github.githubassets.com/images/icons/emoji/'
+      }
+    end
+
+    def on_handle_html(content)
+      emoji_filter(content, 'pre code') do |emoji|
+        # mark current file has been handled
+        self.handled = true
+
+        # here is the replacement content
+        "<img class=\"#{config['css']['class']}\""\
+          " title=\":#{emoji.name}:\""\
+          " alt=\":#{emoji.name}:\""\
+          " raw=\"#{emoji.raw}\""\
+          " src=\"#{config['src']}#{emoji.image_filename}\""\
+          " style=\"vertical-align: middle; display: inline;"\
+          " max-width: 1em; visibility: hidden;\""\
+          " onload=\"this.style.visibility='visible'\""\
+          " onerror=\"this.replaceWith(this.getAttribute('raw'))\">"\
+          "</img>"
+      end
+    end
+
+    def emoji_filter(content, selector)
+      # use nokogiri to parse html
+      doc = Nokogiri::HTML(content)
+
+      body = doc.at('body')
+
+      # in case of a page has no the body node, especially when your
+      # page's layout field of front matter is nil or unavailable
+      return content if body.nil?
+
+      # filter nodes (pre, code)
+      nodes = body.css(selector)
+      nodes.each do |node|
+        # handle emoji markup
+        node.inner_html = node.inner_html.gsub(
+          /:([\w\d+-]+?):/, '\:\1\:'
+        )
+      end
+
+      # parse the emoji
+      content = body.inner_html
+      content.scan(/(?<!\\):([\w\d+-]+?)(?<!\\):/) do |match|
+        # Skip invalid emoji name
+        emoji = Emoji.find_by_alias match[0]
+        next if emoji.nil?
+
+        # escape plus sign
+        emoji_name = emoji.name.gsub('+', '\\\+')
+
+        result = yield emoji
+        next if result.nil?
+
+        content = content.gsub(
+          /(?<!\=")\s*:#{emoji_name}:\s*(?!"\s)/,
+          result)
+      end
+
+      body.inner_html = content
+
+      # restore nodes (pre, code)
+      nodes.each do |node|
+        # handle emoji markup
+        node.inner_html = node.inner_html.gsub(
+          /\\:([\w\d+-]+?)\\:/, ':\1:'
+        )
+      end
+
+      doc.to_html
+    end
+  end
+end
