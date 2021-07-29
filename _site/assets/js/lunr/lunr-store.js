c270c01e15e6713043e34f3c813b21a7d00f6acd
Julien Guégan <julienguegan56520@gmail.com>
2021-07-29 20:40:35 +0200

    update post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,13 +1,7 @@
 var store = [{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["blog"],
-        "tags": ["Jekyll","Template","Help","First post"],
-        "url": "/blog/welcome-to-jekyll/",
-        "teaser": null
-      },{
         "title": "Créer un blog Jekyll : Ruby, Markdown, Github, Latex",
         "excerpt":"J’écris le 1er post de ce blog pour parler tout simplement de comment créer un blog comme celui-ci. La raison principale qui m’a poussé à utiliser Jekyll pour créer un blog est la possibilité d’écrire facilement des équations \\(\\LaTeX\\). En effet, j’avais auparavant essayé avec WordPress mais aucune des solutions...","categories": ["blog"],
-        "tags": ["Jekyll","Markdown","Github","Latex","Blog"],
+        "tags": ["Jekyll","Ruby","Markdown","Kramdown","Github","Latex","Katex","Blog"],
         "url": "/blog/creer_un_blog/",
         "teaser": null
       }]
