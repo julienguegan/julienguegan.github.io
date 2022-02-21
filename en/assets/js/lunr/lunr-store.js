a527d9aaf90f35564a96c3051111e6470f194ea4
julienguegan <julienguegan@users.noreply.github.com>
2022-02-21 23:05:39 +0000

    Deploy site built from commit d9af5416fd8c1735e458d75648daf019b00aa051

---
+++ en/assets/js/lunr/lunr-store.js
@@ -0,0 +1,37 @@
+var store = [{
+        "title": "Make a blog with Jekyll : Markdown, Github, Latex",
+        "excerpt":"I write the 1st post of this blog to simply talk about how to create a blog like this. The main reason that made me use Jekyll to create a blog is the ability to easily write $\\LaTeX$ equations. Indeed, I had previously tried with WordPress but none of the...","categories": ["blog"],
+        "tags": ["Jekyll","Ruby","Markdown","Kramdown","Github","Latex","Katex","Blog"],
+        "url": "/posts/en/2021-07-25-creer_un_blog/",
+        "teaser": "/assets/images/teaser_blog_jekyll.png"
+      },{
+        "title": "Fractal Object: Dimension, Self-Similarity, Infinity",
+        "excerpt":"They are present in tropical forests, at the forefront of medical research, in films and everywhere where wireless communication reigns. This mystery of nature has finally been unraveled. “Damn, but it’s of course !”. Perhaps you have never heard of these strange shapes, yet they are everywhere around you. Their...","categories": ["blog"],
+        "tags": ["Fractal","Python","Julia","Mandelbrot","Récursivité","Infini"],
+        "url": "/posts/en/2021-08-02-objet_fractal/",
+        "teaser": "/assets/images/teaser_fractal.jfif"
+      },{
+        "title": "Population dynamics: ODE, ecology, logistics",
+        "excerpt":"Among the challenges of the 21th century, ecology has a major role since it is the science that studies the interactions of living beings with each other and with their environment. To model these interactions, population dynamics is the branch that is interested in the demographic fluctuations of species. Its...","categories": ["blog"],
+        "tags": ["équations différentielles ordinaires","écologie","modélisation","équation logistique"],
+        "url": "/posts/en/2021-08-11-dynamique_des_populations/",
+        "teaser": "/assets/images/teaser_dynamique_population.jpg"
+      },{
+        "title": "Optimization: algorithm, XFOIL, airfoil",
+        "excerpt":"In everyday life, we often seek to optimize our actions to make the least effort and well, in the world of engineering, it’s the same thing. Minimization problems are ubiquitous in many systems whether to save time, money, energy, raw material, or even satisfaction. For example, we can seek to...","categories": ["blog"],
+        "tags": ["optimisation","gradient","heuristique","aérodynamisme","modélisation"],
+        "url": "/posts/en/2021-08-21-optimisation_profil_aile/",
+        "teaser": "/assets/images/teaser_airflow.jpg"
+      },{
+        "title": "Neural Network: statistics, gradient, perceptron",
+        "excerpt":"In recent years, we hear more and more in the media the words: artificial intelligence, neural network, Deep Learning … Indeed, many innovations have emerged thanks to these technologies but that this hides really behind this terminology? Ever since the first programmable computers were designed, people have been amazed to...","categories": ["blog"],
+        "tags": ["machine learning","deep learning"],
+        "url": "/posts/en/2021-09-10-reseau_de_neurone/",
+        "teaser": "/assets/images/teaser_neural_network.jpg"
+      },{
+        "title": "CNN : convolution, Pytorch, Deep Dream",
+        "excerpt":"Convolutional neural networks (CNN) are the models that have made it possible to make a leap forward in image recognition problems. They are at the heart of many applications ranging from facial identification security systems to the classification of your vacation photos, including synthetic face generation and Snapchat filters. One...","categories": ["blog"],
+        "tags": ["machine learning","deep learning","convolution","deep dream"],
+        "url": "/posts/en/2022-01-01-convolution_deepdream/",
+        "teaser": "/assets/images/teaser_deepdream.jpg"
+      }]
